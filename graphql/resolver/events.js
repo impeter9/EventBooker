1b594edb5c18596ab5ca49f98a9ddd5fdc949920
impeter9 <impeter9@gmail.com>
2020-06-01 19:25:55 -0700

    implement user authentication using jwt to generate token

--- graphql/resolver/events.js
+++ graphql/resolver/events.js
@@ -1,5 +1,8 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
+
 const { transformEvent } = require('./merge');
+
 
 module.exports = {
     events: async () => {
@@ -8,21 +11,22 @@ module.exports = {
             const events = await Event.find();
             // filter metadata from mongo with map
             return events.map(event => {
-                // return { ...event._doc, _id: event._doc._id.toString() };
-                // return { ...event._doc, _id: event.id };
                 return transformEvent(event);
             })
         } catch (err) {
             throw err;
         }
     },
-    createEvent: async (args) => {
+    createEvent: async (args, req) => { // gets request argument automatically
+        if (!req.isAuth) { // make sure the user is authenticated for creating events
+            throw new Error('Unauthenticated!');
+        }
         const event = new Event({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
             date: new Date(args.eventInput.date),
-            creator: '5ed32e98f09e5218cb583c18'
+            creator: req.userId
         });
         let createdEvent;
         try {
@@ -31,7 +35,7 @@ module.exports = {
             // bind creates/return the function but avoid to be called before passing down (avoid infinite loop)
             // graphql will only execute the function when user asks for that field ex) creator in this case
             createdEvent = transformEvent(result);;
-            const creator = await User.findById('5ed32e98f09e5218cb583c18');
+            const creator = await User.findById(req.userId);
             // add transaction for atomicity
             if (!creator) {
                 throw new Error('User not found.');
