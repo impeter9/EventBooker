3b737733df0f570adcc77d44b5f41f1ca44f169e
impeter9 <impeter9@gmail.com>
2020-06-03 16:23:42 -0700

    add authentication forms and its css

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component {
     render() {
-        return (<h1>The Auth Page</h1>);
+        return (
+            <form className="auth-form">
+                <div className="form-control">
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <div className="form-actions">
+                    <button type="submit">Submit</button>
+                    <button type="button">Signup</button>
+                </div>
+            </form>
+        )
     }
 }
 
