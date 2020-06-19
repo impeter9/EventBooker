303b2c02ddf3d882e25adbbcdd88bf8ea22cb728
impeter9 <impeter9@gmail.com>
2020-06-18 17:12:21 -0700

    refactor graphql query using variables

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -5,21 +5,21 @@ import AuthContext from '../context/auth-context';
 import BookingList from '../components/Bookings/BookingList/BookingList';
 
 class BookingsPage extends Component {
-    state = {
-        isLoading: false,
-        bookings: []
-    }
+  state = {
+    isLoading: false,
+    bookings: [],
+  };
 
-    static contextType = AuthContext;
+  static contextType = AuthContext;
 
-    componentDidMount () {
-        this.fetchBookings();
-    }
+  componentDidMount() {
+    this.fetchBookings();
+  }
 
-    fetchBookings = () => {
-        this.setState({ isLoading: true });
-        const requestBody = {
-            query: `
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
                 query {
                     bookings {
                         _id
@@ -31,76 +31,91 @@ class BookingsPage extends Component {
                         }
                     }
                 }
-            `
-        };
-
-        fetch('http://localhost:8000/graphql', {
-            method: 'POST',
-            body: JSON.stringify(requestBody),
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + this.context.token
-            }
-        }).then(res => {
-            if (res.status !== 200 && res.status !== 201) {
-                throw new Error('Failed!');
-            }
-            return res.json();
-        }).then(resData => {
-            const bookings = resData.data.bookings;
-            this.setState({ bookings, isLoading: false });
-        }).catch(err => {
-            console.log(err);
-            this.setState({ isLoading: false });
-        });
+            `,
     };
 
-    deleteBookingHandler = bookingId => {
-        this.setState({ isLoading: true });
-        const requestBody = {
-            query: `
-                mutation {
-                    cancelBooking(bookingId: "${bookingId}") {
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
+
+  deleteBookingHandler = (bookingId) => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+                mutation CancelBooking($id: ID!) {
+                    cancelBooking(bookingId: $id) {
                         _id
                         title
                     }
                 }
-            `
-        };
+            `,
+      variables: {
+        id: bookingId,
+      },
+    };
 
-        fetch('http://localhost:8000/graphql', {
-            method: 'POST',
-            body: JSON.stringify(requestBody),
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + this.context.token
-            }
-        }).then(res => {
-            if (res.status !== 200 && res.status !== 201) {
-                throw new Error('Failed!');
-            }
-            return res.json();
-        }).then(resData => {
-            this.setState(prevState => {
-                const updatedBookings = prevState.bookings.filter(booking => {
-                    return booking._id !== bookingId;
-                });
-                return { bookings: updatedBookings, isLoading: false };
-            });
-        }).catch(err => {
-            console.log(err);
-            this.setState({ isLoading: false });
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        this.setState((prevState) => {
+          const updatedBookings = prevState.bookings.filter((booking) => {
+            return booking._id !== bookingId;
+          });
+          return { bookings: updatedBookings, isLoading: false };
         });
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                {this.state.isLoading ? <Spinner /> : 
-                    (<BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />)}
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <BookingList
+            bookings={this.state.bookings}
+            onDelete={this.deleteBookingHandler}
+          />
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default BookingsPage;
