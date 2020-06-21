a8dd63a87fb5cfc48412014a9b3017bd0cf35847
impeter9 <impeter9@gmail.com>
2020-06-20 20:40:20 -0700

    edit readme, add flowchart and gif

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# graphql-react-event-booking
+# EventBooker
+
+Event booking application.
+
+## Project Flowchart
+
+![flowchart](https://eventbooker.s3-us-west-1.amazonaws.com/eventbooker-flowchart.png)
+
+## Screen Capture
+
+![Imgur Image](./EventBooker.gif)
+
+## Setup to run locally
+
+Clone repo to local machine.
+
+From within the root directory of repo run the following commands:
+
+```sh
+npm install
+npm run start
+npm run frontend
+```
+
+Navigate to link in a web browser.
+
+> http://localhost:3000
--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -4,37 +4,40 @@ import { NavLink } from 'react-router-dom';
 import AuthContext from '../../context/auth-context';
 import './MainNavigation.css';
 
-const MainNavigation = props => (
-    <AuthContext.Consumer>
-        {(context) => {
-            return (
-                <header className="main-navigation">
-                    <div className="main-navigation__logo">
-                        <h1>EasyEvent</h1>
-                    </div>
-                    <div className="main-navigation__items">
-                        <ul>
-                            {!context.token && <li>
-                                <NavLink to="/auth">Authenticate</NavLink>
-                            </li>}
-                            <li>
-                                <NavLink to="/events">Events</NavLink>
-                            </li>
-                            {context.token && 
-                            <React.Fragment>
-                                <li>
-                                    <NavLink to="/bookings">Bookings</NavLink>
-                                </li>
-                                <li>
-                                    <button onClick={context.logout}>Logout</button>
-                                </li>
-                            </React.Fragment>}
-                        </ul>
-                    </div>
-                </header>
-            )
-        }}
-    </AuthContext.Consumer>
+const MainNavigation = (props) => (
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>EventBooker</h1>
+          </div>
+          <div className="main-navigation__items">
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authenticate</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && (
+                <React.Fragment>
+                  <li>
+                    <NavLink to="/bookings">Bookings</NavLink>
+                  </li>
+                  <li>
+                    <button onClick={context.logout}>Logout</button>
+                  </li>
+                </React.Fragment>
+              )}
+            </ul>
+          </div>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
 );
 
 export default MainNavigation;
